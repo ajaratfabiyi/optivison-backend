784d2f4395b177be5c85903b8acbca311d64cf22
ajaratfabiyi <ajaratfabiyi2001@gmail.com>
2025-08-16 16:00:20 +0100

    Update admin.py

--- kyc/admin.py
+++ kyc/admin.py
@@ -1,8 +1,38 @@
 from django.contrib import admin
 from .models import KYCSubmission
 
+
 @admin.register(KYCSubmission)
 class KYCSubmissionAdmin(admin.ModelAdmin):
-    list_display = ('user', 'document_type', 'status', 'submitted_at', 'reviewed_at')
-    list_filter = ('status', 'document_type')
-    search_fields = ('user__email',)
+    list_display = (
+        'id',
+        'user',
+        'document_type',
+        'status',
+        'submitted_at',
+        'reviewed_at',
+    )
+    list_filter = ('status', 'document_type', 'submitted_at', 'reviewed_at')
+    search_fields = ('user__email', 'user__username')
+    ordering = ('-submitted_at',)
+    readonly_fields = ('submitted_at', 'reviewed_at')
+
+    fieldsets = (
+        (None, {
+            'fields': (
+                'user',
+                'document_type',
+                'document_front',
+                'document_back',
+                'selfie',
+                'status',
+                'rejection_reason',
+            )
+        }),
+        ('Timestamps', {
+            'fields': (
+                'submitted_at',
+                'reviewed_at',
+            )
+        }),
+    )
