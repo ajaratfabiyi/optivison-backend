be2600835ef348da71fdf3798f6056043ec53b68
ajaratfabiyi <ajaratfabiyi2001@gmail.com>
2025-08-16 16:03:20 +0100

    Update urls.py

--- kyc/urls.py
+++ kyc/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from .views import SubmitKYCView, KYCStatusView, AdminKYCUpdateView
 
 urlpatterns = [
+    # User endpoints
     path('submit/', SubmitKYCView.as_view(), name='submit_kyc'),
     path('status/', KYCStatusView.as_view(), name='kyc_status'),
+
+    # Admin endpoint for approve/reject
     path('admin/update/<int:pk>/', AdminKYCUpdateView.as_view(), name='admin_update_kyc'),
 ]
