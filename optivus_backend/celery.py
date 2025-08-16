1bc2d06ae8c11a76a349d6816bb4f3c1dedcc3a6
ajaratfabiyi <ajaratfabiyi2001@gmail.com>
2025-08-16 16:11:07 +0100

    Update celery.py

--- optivus_backend/celery.py
+++ optivus_backend/celery.py
@@ -4,14 +4,15 @@ from celery import Celery
 # Set default Django settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'optivus_backend.settings')
 
+# Create Celery instance
 app = Celery('optivus_backend')
 
-# Load config from Django settings, using namespace CELERY
+# Load config from Django settings, using CELERY_* namespace
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
-# Auto-discover tasks from all registered Django app configs
+# Auto-discover tasks in all registered Django apps
 app.autodiscover_tasks()
 
 @app.task(bind=True)
 def debug_task(self):
-    print(f'Request: {self.request!r}')
+    print(f'[CELERY DEBUG] Request: {self.request!r}')
